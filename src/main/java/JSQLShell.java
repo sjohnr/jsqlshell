68edad7571c6ba2ac403531d82facc28f42a603d
Stephen Riesenberg <sjohnr@hotmail.com>
2014-11-19 23:04:24 -0600

    Minor change to JSQLShell along with previous refactor

--- src/main/java/JSQLShell.java
+++ src/main/java/JSQLShell.java
@@ -4,7 +4,7 @@ import net.rakowicz.jsqlshell.SqlShellApp;
 public class JSQLShell {
 
     public static void main(String[] args) {
-        new SqlShellApp(args.length > 0 ? args[0] : null);
+        new SqlShellApp(args.length > 0 ? args[0] : null).run();
     }
 
 }
